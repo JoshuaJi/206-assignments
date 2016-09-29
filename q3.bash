a3c3da2f593aeb3605443ed9f75a0299c9e5ce76
Xu JI <xji12@ds2.CS.McGill.CA>
2016-09-28 23:39:14 -0400

    add question 3

--- q3.bash
+++ q3.bash
@@ -1,17 +1,65 @@
 #!/bin/bash
 
-FILE_LIST=`ls *.dat`
+FILE_LIST_DAT=($(ls *.dat))
+FILE_LIST_JPG=($(ls *.jpg))
+
+if [[ $# != 1 ]]; then
+	echo "please enter one argument [alpha], [weight], [length]"
+	exit
+fi
 
 case $1 in
 	"alpha" )
-		for FILE_NAME in $FILE_LIST; do
-			TEMP_ARRAY=(${FILE_NAME//./})
-			echo "${TEMP_ARRAY[@]}"
-		done;;
+		for FILE_NAME in ${FILE_LIST_DAT[@]}; do
+			echo ${FILE_NAME%%.*}
+		done
+		convert *.jpg -append alpha.png
+		eog alpha.png
+		;;
 
 	"weight" )
+		WEIGHTS=($(cat *.dat|cut -d' ' -f1))
+
+		SORTED_WEIGHTS=($(cat *.dat | cut -d' ' -f1 | sort -n))
+
+		ORDER=( )
+		ORDERED_FILES=( )
+
+		for SORTED_WEIGHT in ${SORTED_WEIGHTS[@]}; do
+			for (( i=0 ; i<${#WEIGHTS[@]} ; i++)); do
+				if [ "${SORTED_WEIGHT}" = "${WEIGHTS[$i]}" ]; then
+					ORDER+=($i)
+				fi
+			done
+		done
+		for INDEX in ${ORDER[@]}; do
+			ORDERED_FILES+=(${FILE_LIST_JPG[$INDEX]})
+			echo ${FILE_LIST_DAT[$INDEX]%%.*}
+		done
+		convert ${ORDERED_FILES[@]} -append weight.png
+		eog weight.png
 		;;
 
 	"length" )
+		LENGTHS=($(cat *.dat|cut -d' ' -f2))
+		SORTED_LENGTHS=($(cat *.dat | cut -d' ' -f2 | sort -n))
+
+		ORDER=( )
+		ORDERED_FILES=( )
+
+		for SORTED_LENGTH in ${SORTED_LENGTHS[@]}; do
+			for (( i=0 ; i<${#LENGTHS[@]} ; i++)); do
+				if [ "${SORTED_LENGTH}" = "${LENGTHS[$i]}" ]; then
+					ORDER+=($i)
+				fi
+			done
+		done
+		for INDEX in ${ORDER[@]}; do
+			ORDERED_FILES+=(${FILE_LIST_JPG[$INDEX]})
+			echo ${FILE_LIST_DAT[$INDEX]%%.*}
+		done
+		convert ${ORDERED_FILES[@]} -append length.png
+                eog length.png
+
 		;;
 esac
