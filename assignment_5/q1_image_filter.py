548886b75fb00c57c8369e7d6e9c9bff35ff8220
JohsuaJi <JiXu204@gmail.com>
2016-11-28 11:09:23 -0500

    q1 done

--- assignment_5/q1_image_filter.py
+++ assignment_5/q1_image_filter.py
@@ -6,11 +6,11 @@ clib = ctypes.CDLL("./libfast_filter.so")
 if len(sys.argv) != 13:
 	sys.exit(0)
 # arguments
-input_img_path = open(sys.argv[1], 'rb').read()
-output_img_data = ' ' * len(input_img_path)
-print input_img_path
+input_img_path = open(sys.argv[1], 'rb')
+input_img_data = input_img_path.read()
+output_img_data = ' ' * len(input_img_data)
 output_img_path = open(sys.argv[2], 'wb')
-width = sys.argv[3]
+width = int(sys.argv[3])
 weights = sys.argv[4:]
 weights = map(float, weights)
 
@@ -18,5 +18,8 @@ CFloatArrayType = ctypes.c_float * len(weights)
 cfloat_array_instance = CFloatArrayType( *weights )
 
 # invocking c library function
-clib.doFiltering( input_img_path, cfloat_array_instance, width, output_img_data)
+clib.doFiltering( ctypes.c_char_p(input_img_data), cfloat_array_instance, width, ctypes.c_char_p(output_img_data))
+output_img_path.write(output_img_data)
+input_img_path.close()
+output_img_path.close()
 # print output_img_data
