ec2d743866b022a07ac602ab31f90dee27698815
JohsuaJi <JiXu204@gmail.com>
2016-11-28 04:07:47 -0500

    q1

--- assignment_5/q1_image_filter.py
+++ assignment_5/q1_image_filter.py
@@ -1,21 +1,22 @@
 import ctypes
 import sys
 
-clib = ctypes.cdll.LoadLibrary("libfast_filter.so")
+clib = ctypes.CDLL("./libfast_filter.so")
 
 if len(sys.argv) != 13:
 	sys.exit(0)
 # arguments
-input_img_path = sys.argv[1]
-output_img_path = sys.argv[2]
+input_img_path = open(sys.argv[1], 'rb').read()
+output_img_data = ' ' * len(input_img_path)
+print input_img_path
+output_img_path = open(sys.argv[2], 'wb')
 width = sys.argv[3]
 weights = sys.argv[4:]
+weights = map(float, weights)
 
-print input_img_path
-print output_img_path
-print width
-print weights
+CFloatArrayType = ctypes.c_float * len(weights)
+cfloat_array_instance = CFloatArrayType( *weights )
 
 # invocking c library function
-#clib.doFiltering( unsigned char* img_data, float* filter_weights,       
-#                  int filter_width,        unsigned char* out_img_data )
+clib.doFiltering( input_img_path, cfloat_array_instance, width, output_img_data)
+# print output_img_data
