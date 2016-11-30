2bca13b44598cc90eb2382afe99afe3ceb377767
JohsuaJi <JiXu204@gmail.com>
2016-11-29 19:19:18 -0500

    q2 done

--- assignment_5/q1_image_filter.py
+++ assignment_5/q1_image_filter.py
@@ -3,8 +3,24 @@ import sys
 
 clib = ctypes.CDLL("./libfast_filter.so")
 
-if len(sys.argv) != 13:
+number_of_args = 0
+try:
+	number_of_args = 4+int(sys.argv[3])*int(sys.argv[3])
+except Exception as e:
+	print "Usage python q1_image_filter.py <input_file> <output_file> <width> <weights_array>"
 	sys.exit(0)
+
+if len(sys.argv) != number_of_args:
+	print "Usage python q1_image_filter.py <input_file> <output_file> <width> <weights_array>"
+	sys.exit(0)
+
+for x in xrange(0, number_of_args-3):
+	try:
+		float(sys.argv[x + 3])
+	except Exception as e:
+		print "width and weights should be numbers"
+		sys.exit(0)
+		
 # arguments
 input_img_path = open(sys.argv[1], 'rb')
 input_img_data = input_img_path.read()
